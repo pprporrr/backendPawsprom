cac216edf0aa2dc8b713d80045689d8a45935d35
ppr <pprporrr@gmail.com>
2023-11-12 20:22:21 +0700

    test

--- backend/APIs/dbConnector.py
+++ backend/APIs/dbConnector.py
@@ -32,7 +32,7 @@ class DBConnector:
 
 def get_db_connector():
     db_config = {
-        "host": "pawsprom-rds.cw9sptej10m2.us-east-1.rds.amazonaws.com",
+        "host": "pawsprom-rds.cgdgakyqi7xb.us-east-1.rds.amazonaws.com",
         "port": 3306,
         "user": "admin",
         "password": "pawsprom-rds",
